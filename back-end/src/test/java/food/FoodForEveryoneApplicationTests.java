b1574352d943be0149dd9827524268ca4d8aa9d8
MaverickShin <dobinu1102@gmail.com>
2022-07-29 16:43:04 +0900

    프로젝트 생성

---
+++ back-end/src/test/java/food/FoodForEveryoneApplicationTests.java
@@ -0,0 +1,13 @@
+package food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodForEveryoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
